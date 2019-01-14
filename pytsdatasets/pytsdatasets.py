1bc2c559467a69f1c04324f6ff733573bf2d0f74
Mario Juez Gil <mariojg@ubu.es>
2019-01-14 14:16:52 +0100

    added globaltemp dataset

--- pytsdatasets/pytsdatasets.py
+++ pytsdatasets/pytsdatasets.py
@@ -1,6 +1,11 @@
 import os.path
 import pandas as pd
-
 pkgdir = os.path.dirname(__file__)
+
+# Jhonson & Jhonson
 jj = pd.read_csv(f'{pkgdir}/data/jj.csv', index_col=0, parse_dates=True)
 jj.index = jj.index.to_period("Q")
+
+# Global temperatures
+globaltemp = pd.read_csv(f'{pkgdir}/data/globaltemp.csv', index_col=0, 
+                            parse_dates=True)
