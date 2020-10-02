d39579701474d1a7aaa8bbb7fdc7123ab25655d2
mjuez <mariojg@ubu.es>
2020-10-02 14:31:31 +0200

    fixed pypi license label

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setuptools.setup(
         'Topic :: Scientific/Engineering',
         'Topic :: Education',
         'Topic :: Database',
-        'License :: OSI Approved :: GPLv3',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
